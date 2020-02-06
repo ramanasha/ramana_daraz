7833105a578877ae2044503cfc5df0da357929e8
ramanasha <ramanakarasarma@gmail.com>
2020-02-06 15:25:16 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="style.css" />
+<style>
+
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #E323FA;
+}
+.hero-image {
+  background-image: url("img/banner.png");
+  height: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.block {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: #FA2330;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+}
+
+div.gallery {
+  margin: 20px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 90%;
+  height: auto;
+}
+
+div.desc {
+  padding: 20px;
+  text-align: center;
+}
+
+
+</style>
+</head>
+<body>
+<div class="hero-image">
+
+</div>
+<center>
+<div>
+  <div class="gallery">
+<a target="_blank" href="">
+  <img src="" alt="watch" width="550" height="350">
+</a>
+<div class="desc">Item -1</div>
+</div>
+
+<div class="gallery">
+<a target="_blank" href="">
+  <img src="" alt="Men" width="550" height="350">
+</a>
+<div class="desc">Item -2</div>
+</div>
+
+<div class="gallery">
+<a target="_blank" href="">
+  <img src="" alt="Kids" width="550" height="350">
+</a>
+<div class="desc">Item -3</div>
+</div>
+
+<div class="gallery">
+<a target="_blank" href="">
+  <img src="" alt="Wemen" width="550" height="350">
+</a>
+<div class="desc">Item -4</div>
+</div>
+<div class="gallery">
+<a target="_blank" href="">
+  <img src="" alt="Mobils" width="550" height="350">
+</a>
+<div class="desc">Item -5</div>
+</div>
+<div class="gallery">
+<a target="_blank" href="">
+  <img src="" alt="Others" width="550" height="350">
+</a>
+<div class="desc">Item -6</div>
+</div>
+
+
+</div>
+
+
+</center>
+<button type="button" class="block">Fashion Item</button>
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="img/img-1.jpg">
+                        <img class="pic-2" src="img/img-2.jpg">
+                    </a>
+
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">20%</span>
+                </div>
+
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Blouse</a></h3>
+                    <div class="price">Rs916.00
+                        <span>Rs920.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="img/img-3.jpg">
+                        <img class="pic-2" src="img/img-4.jpg">
+                    </a>
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">50%</span>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Men's Plain Tshirt</a></h3>
+                    <div class="price">Rs5.00
+                        <span>Rs10.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="img/img-5.jpg">
+                        <img class="pic-2" src="img/img-6.jpg">
+                    </a>
+
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">50%</span>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Men's Plain Tshirt</a></h3>
+                    <div class="price">$5.00
+                        <span>$10.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="img/img-7.jpg">
+                        <img class="pic-2" src="img/img-8.jpg">
+                    </a>
+
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">50%</span>
+                </div>
+
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Men's Plain Tshirt</a></h3>
+                    <div class="price">$5.00
+                        <span>$10.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+
+
+
+
+
+
+
+    </div>
+
+</body>
+</html>
